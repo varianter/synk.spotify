bc93d46aa37a0a89b07ee2d410e5a72219316715
Sindre Aarstrand <saarstrand@akvagroup.com>
2023-05-07 11:10:38 +0200

    Implement recently played store

--- RecentlyPlayedStore.cs
+++ RecentlyPlayedStore.cs
@@ -1,11 +1,27 @@
+using Npgsql;
+
 namespace Synk.Spotify;
 
-public class RecentlyPlayedStore
+internal class RecentlyPlayedStore
 {
-    public Task<IEnumerable<RecentlyPlayed>> AddRecentlyPlayed(IEnumerable<RecentlyPlayed> recentlyPlayed)
+    private readonly CockroachDbContext dbContext;
+
+    internal RecentlyPlayedStore()
     {
-        throw new NotImplementedException();
+        dbContext = new();
+    }
+
+    internal Task AddRecentlyPlayed(IEnumerable<RecentlyPlayed> recentlyPlayed)
+    {
+        using var connection = dbContext.GetConnection();
+
+        var command = new NpgsqlCommand("INSERT INTO recentlyPlayed (userId, trackId, playedAt) VALUES (@userId, @trackId, @playedAt)", connection);
+        command.Parameters.AddWithValue("userId", recentlyPlayed.Select(x => x.UserId));
+        command.Parameters.AddWithValue("trackId", recentlyPlayed.Select(x => x.TrackId));
+        command.Parameters.AddWithValue("playedAt", recentlyPlayed.Select(x => x.PlayedAt));
+
+        return command.ExecuteNonQueryAsync();
     }
 }
 
-public record RecentlyPlayed(string UserId, string TrackId, DateTime PlayedAt);
+internal record RecentlyPlayed(string UserId, string TrackId, DateTime PlayedAt);
