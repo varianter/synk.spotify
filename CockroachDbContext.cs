443f6f13ff0eab2ba0a8dcc00c77eb718f5c5408
Sindre Aarstrand <saarstrand@akvagroup.com>
2023-05-08 11:56:42 +0200

    Fix db context

--- CockroachDbContext.cs
+++ CockroachDbContext.cs
@@ -5,11 +5,33 @@ namespace Synk.Spotify;
 public class CockroachDbContext
 {
     private readonly string connectionString;
+
     public CockroachDbContext()
     {
-        connectionString = Environment.GetEnvironmentVariable("COCKROACHDB_CONNECTION_STRING")
-            ?? throw new("COCKROACHDB_CONNECTION_STRING environment variable not set");
+        var host = Environment.GetEnvironmentVariable("COCKROACHDB_HOST")
+            ?? throw new("COCKROACHDB_HOST environment variable not set");
+        var user = Environment.GetEnvironmentVariable("COCKROACHDB_USER")
+            ?? throw new("COCKROACHDB_USER environment variable not set");
+        var password = Environment.GetEnvironmentVariable("COCKROACHDB_PASSWORD")
+            ?? throw new("COCKROACHDB_PASSWORD environment variable not set");
+
+        connectionString = new NpgsqlConnectionStringBuilder
+        {
+            Host = host,
+            Port = 26257,
+            SslMode = SslMode.VerifyFull,
+
+            Username = user,
+            Password = password,
+            Database = "synk",
+            ApplicationName = "synk.spotify"
+        }.ToString();
     }
 
-    public NpgsqlConnection GetConnection() => new(connectionString);
+    public NpgsqlConnection GetOpenConnection()
+    {
+        var connection = new NpgsqlConnection(connectionString);
+        connection.Open();
+        return connection;
+    }
 }
