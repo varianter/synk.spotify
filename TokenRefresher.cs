23cd561aa8ce4c61a67adf5778e39213e19a96a4
Sindre Aarstrand <saarstrand@akvagroup.com>
2023-05-08 11:53:12 +0200

    Fix token refresher - Use PKCE refresh flow and use default encoding

--- TokenRefresher.cs
+++ TokenRefresher.cs
@@ -8,16 +8,17 @@ internal class TokenRefresher
     private readonly HttpClient client = new();
 
     private const string TokenEndpoint = "https://accounts.spotify.com/api/token";
+    private readonly string clientId;
     private readonly string authHeaderValue;
 
     internal TokenRefresher()
     {
-        var clientId = Environment.GetEnvironmentVariable("SPOTIFY_CLIENT_ID")
-            ?? throw new("SPOTIFY_CLIENT_ID environment variable not set");
+        clientId = Environment.GetEnvironmentVariable("SPOTIFY_CLIENT_ID")
+           ?? throw new("SPOTIFY_CLIENT_ID environment variable not set");
         var clientSecret = Environment.GetEnvironmentVariable("SPOTIFY_CLIENT_SECRET")
             ?? throw new("SPOTIFY_CLIENT_SECRET environment variable not set");
 
-        authHeaderValue = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}"));
+        authHeaderValue = Convert.ToBase64String(Encoding.Default.GetBytes($"{clientId}:{clientSecret}"));
     }
 
     internal async Task<Token?> RefreshTokenAsync(Token token)
@@ -25,7 +26,8 @@ internal class TokenRefresher
         var form = new Dictionary<string, string>
         {
             ["grant_type"] = "refresh_token",
-            ["refresh_token"] = token.RefreshToken
+            ["refresh_token"] = token.RefreshToken,
+            ["client_id"] = clientId,
         };
 
         var request = new HttpRequestMessage(HttpMethod.Post, TokenEndpoint)
@@ -51,12 +53,13 @@ internal class TokenRefresher
         return token with
         {
             AccessToken = newTokens.access_token,
-            ExpiresAt = DateTime.UtcNow.AddSeconds(newTokens.expires_in)
+            ExpiresAt = DateTime.UtcNow.AddSeconds(newTokens.expires_in),
+            RefreshToken = newTokens.refresh_token ?? token.RefreshToken
         };
     }
 }
 
 // Ignore naming rule violations for this internal record. It makes deserializing the json simpler.
 #pragma warning disable IDE1006 // Naming Styles
-internal record SpotifyTokenResponse(string access_token, string token_type, string scope, long expires_in, string refresh_token);
+internal record SpotifyTokenResponse(string access_token, string token_type, string scope, long expires_in, string? refresh_token);
 #pragma warning restore IDE1006 // Naming Styles
